86ca80266a22f175bcd561ae965d4ec65d8678d8
Ekan <ekanessien@gmail.com>
2024-12-12 20:54:33 -0800

    Adjusted Functions for Rock, Paper Scissors

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("puro para delante mi buen doggy"); 
+
+const listSelection = ["rock", "paper", "scissors"];
+let humanScore = 0; 
+let computerScore = 0; 
+
+
+
+let getComputerChoice = function() {
+
+    //Fisher Yates Algorithm
+    for (let i = listSelection.length - 1; i > 0; i--) {
+
+       //generate random number
+        const random = Math.floor(Math.random() * (i + 1));
+
+       //random number to string
+        return listSelection[random];
+    } 
+}
+
+console.log(getComputerChoice());
+
+
+let getHumanChoice = function() {
+    let humanInput = prompt("input one --- rock, paper, or scissors");
+    console.log(humanInput); 
+}
+ 
+
+getHumanChoice();
