106f38c48e68c35564f9c576c6f309fea19c7ab9
Ekan <ekanessien@gmail.com>
2025-02-15 12:06:42 -0800

    Game Done

--- index.js
+++ index.js
@@ -1,64 +1,71 @@
-"use strict"; 
+"use strict";
 
-// Declaration
-const options = ["rock", "paper", "scissors"];
-let humanScore = 0;
-let computerScore = 0; 
+//Global Scope
+const choices = ["rock", "paper", "scissors"];
 
-// Computer Choice Function
-const getComputerChoice = function() {
-    // Generate a random number
-    let randomNumber = Math.trunc(Math.random() * 3);
-    let randomChoice = options[randomNumber];
-    return randomChoice; 
-}
-// Human Choice Function
-const getHumanChoice = function() {
-    //Human Input
-    let humanInput = prompt("Choose One---Rock, Paper or Scissors or Leave Blank to Quit");
+//Computer Choice Function
+const getComputerChoice = function () {
+  let randomNum = Math.floor(Math.random() * 3);
+  let computerSelection = choices[randomNum];
+  return computerSelection;
+};
 
-    if (humanInput === null || humanInput === "") {
-        alert("You're a Quitter !!!"); 
-        return null; 
-    } else if (humanInput.toLowerCase() === "rock" ||
-        humanInput.toLowerCase() === "paper" ||
-        humanInput.toLowerCase() === "scissors"
+//Human Choice Function
+const gethumanChoice = function () {
+  let humanInput = prompt("Choose 1: Rock, Paper or Scissors").toLowerCase();
+
+  while (
+    humanInput !== "rock" &&
+    humanInput !== "paper" &&
+    humanInput !== "scissors"
+  ) {
+    alert("Wrong Choice");
+    humanInput = prompt("Choose 1: Rock, Paper or scissors").toLowerCase();
+  }
+  return humanInput;
+};
+
+//Playgame Function
+const playGame = function () {
+  let humanScore = 0;
+  let computerScore = 0;
+
+  //Playround Formula
+  const playRound = function (humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+      console.log("It's a Tie");
+    } else if (
+      (humanChoice === "rock" && computerChoice === "scissors") ||
+      (humanChoice === "paper" && computerChoice === "rock") ||
+      (humanChoice === "scissors" && computerChoice === "paper")
     ) {
-        alert("Good Choice!!!"); 
-        return humanInput.toLowerCase();
+      console.log(`You Win---${humanChoice} beat ${computerChoice}`);
+      humanScore++;
     } else {
-        alert("Invalid Choice--Choose One--Rock, Paper or Scissors");
-        return undefined; 
+      console.log(`You Loose---${computerChoice} beat ${humanChoice}`);
+      computerScore++;
     }
-}
+  };
 
+  //5 Rounds Loop
+  for (let i = 0; i < 5; i++) {
+    let humanChoice = gethumanChoice();
+    let computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+  }
 
+  //Final Sccore Message
+  let finalMessage;
+  if (humanScore === computerScore) {
+    finalMessage = "It's a tie";
+  } else if (humanScore > computerScore) {
+    finalMessage = "You Won";
+  } else {
+    finalMessage = "You Loose";
+  }
+  console.log(
+    `${finalMessage}. Final Score----Human ${humanScore} VS Computer ${computerScore}`
+  );
+};
 
-//AJUSTAR IF STATEMENTS !!!!
-
-const playRound = function(humanChoice, computerChoice) {
-
-    if (humanChoice === "rock" && computerChoice === "scissors") {
-        console.log("You Win---Rock Beats scissors");
-        humanScore++;
-    } else if (humanChoice === "paper" && computerChoice === "rock") {
-        console.log("You Win---Paper Beats rock");
-        humanScore++;
-    } else if (humanChoice === "scissors" && computerChoice === "paper") {
-        console.log("You Win---Scissors Beat paper");
-        humanScore++;
-    } else if (humanChoice === "rock" && computerChoice === "paper") {
-        console.log("You Loose--- Paper Beats rock");
-        computerScore++;
-    } else if (humanChoice === "paper" && computerChoice === "scissors") {
-        console.log("You Loose---Scissors Beat paper");
-        computerScore++;
-    } else {
-        console.log("You Loose---Rock Beats Scissors");
-        computerScore++;
-    }
-}
-
-playRound();
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+playGame();
